7b0603557bd3b757c45527a7633264db5fdfaa89
Danila Solovenko <dansil2301@gmail.com>
2024-03-03 16:25:52 +0100

    added documets for the prokect (should later move them to another place)

--- TradeX/src/main/java/Eco/TradeX/TradeXApplication.java
+++ TradeX/src/main/java/Eco/TradeX/TradeXApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 public class TradeXApplication {
 
 	public static void main(String[] args) {
