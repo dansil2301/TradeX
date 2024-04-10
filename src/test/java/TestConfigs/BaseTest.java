5446a2fd77719363fab149cbedf2946662724e00
Danila Solovenko <dansil2301@gmail.com>
2024-04-11 01:32:16 +0200

    deleted code smaell

--- src/test/java/TestConfigs/BaseTest.java
+++ src/test/java/TestConfigs/BaseTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.ArgumentMatchers.any;
 
-@SpringBootTest
 public class BaseTest {
     @MockBean
     private TraderRepository databaseRepository;
