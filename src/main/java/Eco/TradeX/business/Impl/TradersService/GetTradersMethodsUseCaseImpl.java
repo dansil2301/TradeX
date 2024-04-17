0a7bd6c512f91075dca8282347e39ffe2a0e2570
Danila Solovenko <dansil2301@gmail.com>
2024-04-17 17:32:37 +0200

    now users can get information only with their id

--- src/main/java/Eco/TradeX/business/Impl/TradersService/GetTradersMethodsUseCaseImpl.java
+++ src/main/java/Eco/TradeX/business/Impl/TradersService/GetTradersMethodsUseCaseImpl.java
@@ -1,22 +1,30 @@
 package Eco.TradeX.business.Impl.TradersService;
 
 import Eco.TradeX.business.Interfaces.TraderServiceInterfaces.GetTradersMethodsUseCase;
+import Eco.TradeX.business.exceptions.UnauthorizedDataAccessException;
 import Eco.TradeX.business.utils.TraderUtils.TraderConverter;
+import Eco.TradeX.configuration.security.token.AccessToken;
 import Eco.TradeX.domain.Trader.TraderData;
 import Eco.TradeX.persistence.Impl.TraderRepository.TraderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
 public class GetTradersMethodsUseCaseImpl implements GetTradersMethodsUseCase {
     private final TraderConverter traderConverter;
     private final TraderRepository traderRepository;
+    private AccessToken requestAccessToken;
 
     @Override
     public TraderData getTraderById(Long id) {
+        if (!Objects.equals(requestAccessToken.getId(), id)) {
+            throw new UnauthorizedDataAccessException("STUDENT_ID_NOT_FROM_LOGGED_IN_USER");
+        }
+
         var traderEntity = traderRepository.findById(id);
         return traderConverter.convertToTraderData(traderEntity);
     }
---
+++ src/main/java/Eco/TradeX/business/exceptions/UnauthorizedDataAccessException.java
@@ -0,0 +1,10 @@
+package Eco.TradeX.business.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class UnauthorizedDataAccessException extends ResponseStatusException {
+    public UnauthorizedDataAccessException(String errorCause) {
+        super(HttpStatus.FORBIDDEN, errorCause);
+    }
+}
